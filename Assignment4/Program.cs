750fedfbb250e49cc5b6e34c8f5e26da5d1f7633
visualstudio <visualstudio@122-PC16>
2019-04-05 18:45:30 -0400

    init

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -8,8 +8,49 @@ namespace Assignment4
 {
     class Program
     {
+        ArrayList Beowulf;
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+        }
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+
+        public void ReadTextFiles()
+        {
+
+            // Read file using StreamReader. Read file line by line
+            using (StreamReader file = new StreamReader("//storage/storage$/732052/Documents"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            {
+                foreach (char c in line)
+                {
+                    if (char.IsLetter(c)) { countletters++; }
+                    if (char.IsWhiteSpace(c)) { countSpaces++; }
+                }
+                return countSpaces;
+            }
         }
     }
+}
 }
